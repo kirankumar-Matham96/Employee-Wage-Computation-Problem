3f1399f32d31aeacfe4255248796c00300d9103b
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-17 00:25:04 +0530

    Added: Employee part time use case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,13 +3,22 @@ echo "Wellcome to Employee Wage Computation Program!"
 
 isPresent=1
 isAbsent=0
+isFullTime=1
+isPartTime=2
 empFullTimeHrs=8
+empPartTimeHrs=4
 empWageRatePerHr=20
 attendanceCheck=$((RANDOM%2))
+empHrsCheck=$((1+RANDOM%2))
 
 if [ $isPresent -eq $attendanceCheck ]
 then
-	empWageForTheDay=$(($empFullTimeHrs*$empWageRatePerHr))
+	if [ $isFullTime -eq $empHrsCheck ]
+	then
+		empWageForTheDay=$(($empFullTimeHrs*$empWageRatePerHr))
+	else
+		empWageForTheDay=$(($empPartTimeHrs*$empWageRatePerHr))
+	fi
 else
 	echo "Employee absent!"
 fi
