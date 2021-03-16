26aa9289f2f29dfc22d355673d7a751ed176e92e
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-16 23:34:58 +0530

    Added: Employee attendacne check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 echo "Wellcome to Employee Wage Computation Program!"
 
+isPresent=1
+isAbsent=0
+attendanceCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $attendanceCheck ]
+then
+	echo "Employee present"
+else
+	echo "Employee absent!"
+fi
